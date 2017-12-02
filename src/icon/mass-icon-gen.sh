ec8ddcadc550a540dbaafee82eacdac3148d578b
jrbeverly <jrbeverly@live.com>
2017-12-02 11:41:56 -0500

    Revise icon generation scripts

--- src/icon/mass-icon-gen.sh
+++ src/icon/mass-icon-gen.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+current="$(pwd)"
+icon="$current/viewdocs-autodoc/src/icon/"
 for dir in $(pwd)/*
 do
     test -d "$dir" || continue
-    cd $dir
 
-    echo $dir
-    sh docs/tools/docker-icon.sh
+    sh "$icon"/docker-icon.sh $dir
     #sh docs/tools/docker-icon.sh
     #mv docs/icon/images/icon-256.png docs/icon/icon.png
 done
